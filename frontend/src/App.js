9df138ad90312a3aaf5e9f0dea9b64f6d8ed890c
thazsobral <thazsobral@gmail.com>
2021-09-10 20:25:01 -0300

    add component card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+// import axios from "axios";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import CurrencyCard from "./components/currency-card";
 
 function App() {
+  const [currency, setCurrency] = useState("");
+  const [currencies, setCurrencies] = useState([]);
+
+  function changeCurrency(event) {
+    setCurrency(event.target.value);
+  }
+
+  window.onload = getCurrency;
+
+  function getCurrency() {
+    fetch("https://economia.awesomeapi.com.br/json/all")
+      .then(response => response.json())
+      .then(data => {
+        let currencies = [];
+        for (const key in data) {
+          // <p id="name-${key.toLowerCase()}">${data[key].name}</p>
+          // <p id="value-${key.toLowerCase()}">${key} ${data[key].high}</p>
+          // <label for="${key.toLowerCase()}"></label>
+          // <input type="number" name="${key.toLowerCase()}" id="${key}" value="1" onchange="currencyConverter(this.value, this.id)">
+          currencies.push({
+            name: data[key].name,
+            code: data[key].code,
+            high: data[key].high
+          })
+        };
+        setCurrencies(currencies);
+      })
+  }
+
+
   return (
     <div className="App">
       <Header />
       <main>
         <form>
           <label htmlFor="currency">R$</label>
-          <input type="number" placeholder="Insira a sua quantia aqui" />
+          <input type="number" value={currency} onChange={changeCurrency} placeholder="Insira a sua quantia aqui" />
+          <input type="submit" value="Converter" />
         </form>
-        <CurrencyCard />
+        <div>
+          {currencies.map((currency, key) => {
+            return (
+              <CurrencyCard key={key} name={currency.name} code={currency.code} high={currency.high} />
+            );
+          })
+          }
+        </div>
       </main>
       <Footer />
     </div>
--- frontend/src/components/currency-card/index.js
+++ frontend/src/components/currency-card/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export default function CurrencyCard() {
+export default function CurrencyCard({ key, name, code, high }) {
     return (
         <>
-            <div className="card">
-                <p className="currency-name">Dolar</p>
-                <p className="currency-value">R$5,96</p>
+            <div className="card" key={key}>
+                <p className="currency-name">{name}</p>
+                <p className="currency-code">{code}</p>
+                <p className="currency-value">R$ {high}</p>
                 <p className="currency-value">R$80,00</p>
             </div>
         </>
