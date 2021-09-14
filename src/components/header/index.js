d9fd37f356783c99a5e06a0a21509c2d94970fa9
thazsobral <thazsobral@gmail.com>
2021-09-14 07:22:24 -0300

    fix localization folders

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <>
             <header>
-                <h1>Currenty Converter</h1>
+                <h1>Currency Converter</h1>
                 <p>O Currenty Converter te ajuda a converter seu dinheiro para as moedas de maior visibilidade no mercado atual. E tudo isso de forma simplificada. 😉</p>
             </header>
         </>
