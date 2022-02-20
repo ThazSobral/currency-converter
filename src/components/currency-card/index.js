df287f1fe4401995a0a538736e0b63509b3aca88
thazsobral <30708148+thazsobral@users.noreply.github.com>
2022-02-20 08:04:09 -0300

    Update index.js

--- src/components/currency-card/index.js
+++ src/components/currency-card/index.js
@@ -6,7 +6,7 @@ export default function CurrencyCard({ name, code, high, value }) {
         <>
             <div className="card">
                 <h3 className="currency-name">{name}</h3>
-                <p className="currency-value">R$ 1,00 = {code}$ {parseFloat(high).toFixed(2)}</p>
+                <p className="currency-value">R$ 1.00 = {code}$ {parseFloat(high).toFixed(2)}</p>
                 <p className="currency-converted">R$ {(value/high).toFixed(2)}</p>
             </div>
         </>
